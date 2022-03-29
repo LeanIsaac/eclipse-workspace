7639959ab28b7b165380dc8c7c5a2086aff5da09
LeanIsaac <leancarp09@gmail.com>
2022-03-28 21:21:30 -0300

    update

--- 1-3/src/1-3.c
+++ 1-3/src/1-3.c
@@ -12,28 +12,52 @@
 #include <stdlib.h>
 
 int main(void) {
-	int numeroUno;
-	int numeroDos;
-	int numeroTres;
+	setbuf(stdout,NULL);
+
+	int nro1;
+	int nro2;
+	int nro3;
+	int numeroDelMedio;
 
 	printf("Ingrese el primer numero: ");
-	scanf("%d" , &numeroUno);
+	scanf("%d" , &nro1);
 	printf("Ingrese el segundo numero: ");
-	scanf("%d" , &numeroDos);
-    printf("Ingrese el tercer numero número: ");
-	scanf("%d" , &numeroTres);
+	scanf("%d" , &nro2);
+    printf("Ingrese el tercer número: ");
+	scanf("%d" , &nro3);
 
-    if(numeroUno>numeroDos && numeroDos<numeroTres){
+    /*
+	if(numeroUno>numeroDos && numeroDos<numeroTres){
         printf("el numero del medio es :%d", numeroDos);
     }
     else{
         if(numeroUno<numeroDos && numeroDos < numeroTres){
-            printf("el numero del medio es :%d", numeroUno);
+            printf("el número del medio es :%d", numeroUno);
         }
         else{
-            printf("el numero del medio es :%d", numeroTres);
+            printf("el número del medio es :%d", numeroTres);
         }
     }
+    */
+	if(nro1 != nro2 && nro1 != nro3 && nro2 != nro3){
+		if((nro1 > nro2 && nro1 < nro3) || (nro1 < nro2 && nro1 > nro3)){
+			numeroDelMedio = nro1;
+		}
+		else
+		{
+			if((nro2 > nro1 && nro2 < nro3) || (nro2 < nro1 && nro2 > nro3)){
+				numeroDelMedio = nro2;
+			}
+			else
+			{
+				numeroDelMedio = nro3;
+			}
+		}
+		printf("El número del medio es: %d",numeroDelMedio);
+	}
+	else{
+		printf("No Existe número del medio");
+	}
 	system("pause");
 	return 0;
 }
